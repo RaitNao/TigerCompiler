43645cc45a31820ae2b7e74b52201b3847fde260
dtrinh7 <dtrinh7@gatech.edu>
2018-03-15 23:16:26 -0400

    Autogenerate LL Table java code with python script

--- p1/src/com/tiger/scanner/TokenType.java
+++ p1/src/com/tiger/scanner/TokenType.java
@@ -2,10 +2,10 @@ package com.tiger.scanner;
 
 public enum TokenType {
     COMMENT, ARRAY("array"), BEGIN("begin"), BOOLEAN("boolean"), IF("if"), THEN("then"), ELSE("else"), WHILE("while"), FOR("for"), TO("to"), DO("do"), LET("let"),
-    IN("in"), INT("int"), END("end"), ENDDO("enddo"), ENDIF("endif"), FALSE("false"), TRUE("true"), FLOAT("float"), OF("of"), BREAK("break"), NIL("nil"),
-    FUNC("func"), VAR("var"), TYPE("type"), UNIT("unit"), IMPORT("import"), PRIMITIVE("primitive"), RETURN("return"),
+    IN("in"), INT("int"), END("end"), ENDDO("enddo"), ENDIF("endif"), FALSE("false"), TRUE("true"), FLOAT("float"), OF("of"), BREAK("break"), FUNC("func"),
+    VAR("var"), TYPE("type"), UNIT("unit"), RETURN("return"),
     COMMA(","), COLON(":"), SEMICOLON(";"), LPAREN("("), RPAREN(")"), LSQBR("["), RSQBR("]"), LCBR("{"), RCBR("}"), DOT("."), PLUS("+"), MINUS("-"),
-    STAR("*"), FWSLASH("/"), EQUAL("="), NOTEQUAL("<>"), LESS("<"), LEQ("<="), MORE(">"), MEQ(">="), AND("&"), OR("|"), ASSIGN(":="),
+    STAR("*"), FWSLASH("/"), EQUAL("="), NOTEQUAL("<>"), LESS("<"), MORE(">"), LEQ("<="), MEQ(">="), AND("&"), OR("|"), ASSIGN(":="), UNDERSCORE("_"), EOF("eof"),
     INTLIT, FLOATLIT, IDENTIFIER;
 
     private boolean keyword;
